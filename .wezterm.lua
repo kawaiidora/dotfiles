0b621575815cd71e4b744d6a5b60a735a2c31fef
Dora <kawaiidora@outlook.com>
2023-11-08 07:28:25 +0800

    change color of term

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,10 +8,10 @@ end
 
 config.font = wezterm.font '等距更纱黑体 SC'
 config.font_size = 10.0
-config.color_scheme = 'Ubuntu'
+config.color_scheme = 'Ciapre'
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
-
+-- config.window_background_opacity = 0.85
 config.default_prog = { 'pwsh.exe' }
 
 return config
