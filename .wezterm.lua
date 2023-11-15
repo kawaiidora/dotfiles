44235796668b2f930fe52b8dee4f351a34c2a7ec
Dora <kawaiidora@outlook.com>
2023-11-16 05:13:01 +0800

    use transparent

--- .wezterm.lua
+++ .wezterm.lua
@@ -17,13 +17,13 @@ config.color_scheme = 'BlueDolphin'
 -- config.use_fancy_tab_bar = true
 -- 眼不见为净，只开一个shell时隐藏tab bar
 config.hide_tab_bar_if_only_one_tab = true
--- config.window_background_opacity = 0.7
+config.window_background_opacity = 0.6
 -- 将tab bar放到标题栏，但是wezterm的tab bar太丑了
 -- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.max_fps = 120
 -- config.default_cursor_style = 'SteadyUnderline'
 -- 标题栏亚克力效果
--- config.win32_system_backdrop = "Acrylic"
+config.win32_system_backdrop = "Acrylic"
 config.default_prog = { 'pwsh.exe' }
 
 return config
