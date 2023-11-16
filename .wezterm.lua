05d07d87341a08700a6a0e6d4b9d1389b6363efd
Dora <kawaiidora@outlook.com>
2023-11-16 09:14:09 +0800

    早知道还是gruvbox

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,18 +12,20 @@ config.font = wezterm.font_with_fallback {
     'Consolas',
 }
 
-config.font_size = 10.5
-config.color_scheme = 'BlueDolphin'
--- config.use_fancy_tab_bar = true
+config.font_size = 10
+config.color_scheme = 'GruvboxLight'
+-- config.color_scheme = 'GruvboxDark'
+config.use_fancy_tab_bar = false
 -- 眼不见为净，只开一个shell时隐藏tab bar
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.6
+-- config.window_background_opacity = 0.5
 -- 将tab bar放到标题栏，但是wezterm的tab bar太丑了
 -- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.max_fps = 120
 -- config.default_cursor_style = 'SteadyUnderline'
 -- 标题栏亚克力效果
-config.win32_system_backdrop = "Acrylic"
+-- config.win32_system_backdrop = "Acrylic"
+config.force_reverse_video_cursor = true
 config.default_prog = { 'pwsh.exe' }
 
 return config
