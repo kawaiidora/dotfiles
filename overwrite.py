c62ef14e15a8dae4d1c0b290ea4dac58552878f7
Dora <kawaiidora@outlook.com>
2023-11-21 13:59:32 +0800

    asyncrun

--- overwrite.py
+++ overwrite.py
@@ -2,14 +2,10 @@ import os
 from pathlib import Path
 from datetime import datetime
 
-
-timestamp = datetime.now().strftime('%Y%m%d%H%M%S%f')
-is_windows = os.name == 'nt'
-
 repo = Path(__file__).parent
+is_windows = os.name == 'nt'
+timestamp = datetime.now().strftime('%Y%m%d%H%M%S%f')
 
-vim_folder = 'vimfiles' if is_windows else '.vim'
-vim_target = Path.home().joinpath(vim_folder)
 
 def backup_replace(source: Path, target: Path):
     if target.exists(follow_symlinks=False):
@@ -18,9 +14,12 @@ def backup_replace(source: Path, target: Path):
     is_directory = target.suffix == ''
     target.symlink_to(source, target_is_directory=is_directory)
 
+
 if __name__ == '__main__':
     # repo中的配置文件夹名称固定
+    vim_folder = 'vimfiles' if is_windows else '.vim'
     vim_source = repo.joinpath('vim')
+    vim_target = Path.home().joinpath(vim_folder)
     backup_replace(vim_source, vim_target)
     alacritty_source = repo.joinpath('alacritty')
     appdata = Path(os.path.expandvars('%APPDATA%'))
